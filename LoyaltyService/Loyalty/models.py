8197b4b22ca51e61335b574b2d816efbdfe286ba
sohaibssb <sohayb.salem2012@gmail.com>
2023-03-30 06:11:21 +0300

    Update

--- LoyaltyService/Loyalty/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-import uuid
-
-class UserLoyalty(models.Model):
-    username=models.CharField(max_length=200)
-    reservationCount=models.IntegerField(default=0)
-    status=models.CharField(max_length=10,default='BRONZE')
-    discount=models.IntegerField()
--- PaymentService/Payment/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Payment
-import uuid
-
-class PaymentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Payment
-        fields='__all__'
